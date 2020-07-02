81a75d78f31505b68f92505bde0eea26181efea3
cjshinningg <784319417@qq.com>
2020-07-02 23:23:14 +0800

    8-4 代理模式

--- src/index.js
+++ src/index.js
@@ -121,22 +121,103 @@
 //     alert(100);
 // }
 
-function log(target, name, descriptor){
-    let oldValue = descriptor.value;
-    descriptor.value = function(){
-        console.log(`calling ${name} width ${arguments}`);
-        return oldValue.apply(this, arguments);
-    }
-    return descriptor;
+// function log(target, name, descriptor){
+//     let oldValue = descriptor.value;
+//     descriptor.value = function(){
+//         console.log(`calling ${name} width ${arguments}`);
+//         return oldValue.apply(this, arguments);
+//     }
+//     return descriptor;
+// }
+
+// class Math {
+//     @log
+//     add(a, b){
+//         return a + b;
+//     }
+// }
+
+// let math = new Math();
+// const result = math.add(2, 4);
+// console.log(result);
+
+
+// 代理模式
+// 介绍：
+// 使用者无法访问目标对象
+// 中间加代理，通过代理做授权和控制
+
+// 示例：
+// 科学上网，访问github
+// 明星经纪人
+// class RealImg {
+//     constructor(filename){
+//         this.filename = filename;
+//         this.loadFromDisk();
+//     }
+//     display(){
+//         console.log('display...' + this.filename);
+//     }
+//     loadFromDisk(){
+//         console.log('loading...' + this.filename);
+//     }
+// }
+
+// class ProxyImg {
+//     constructor(filename){
+//         this.realImg = new RealImg(filename);
+//     }
+//     display(){
+//         this.realImg.display();
+//     }
+// }
+
+// let proxyImg = new ProxyImg('1.png');
+// proxyImg.display();
+
+// 明星
+let star = {
+    name: '张xx',
+    age: 25,
+    phone: '13800001111'
 }
 
-class Math {
-    @log
-    add(a, b){
-        return a + b;
+// 经纪人
+let agent = new Proxy(star, {
+    get: function(target, key){
+        if(key === 'phone'){
+            // 返回经纪人自己的电话
+            return '16890908787';
+        }
+        if(key === 'price'){
+            // 明星不报价，经纪人报价
+            return 120000;
+        }
+        return target[key];
+    },
+    set: function(target, key, val){
+        if(key === 'customPrice'){
+            if(val < 100000){
+                throw new Error('价格太低');
+            }else{
+                target[key] = val;
+                return true;
+            }
+        }
     }
-}
+})
 
-let math = new Math();
-const result = math.add(2, 4);
-console.log(result);
+console.log(agent.name);
+console.log(agent.age);
+console.log(agent.phone);
+console.log(agent.price);
+
+agent.customPrice = 90000;
+
+// 代理模式 vs 适配器模式
+// 适配器模式：提供一个不同的接口
+// 代理模式：提供一模一样的接口
+
+// 代理模式 vs 装饰器模式
+// 装饰器模式：扩展功能，原有功能不变可直接使用
+// 代码里模式：显示原有功能，但是经过限制或阉割之后的
